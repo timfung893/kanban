3316442b870af78513c0ee0eeb7ffcbb4a739c42
thinh <thinh.phung@apacpro.com>
2022-11-08 11:17:22 +0700

    add kanban

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import Kanban from './kanban';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Kanban Board</h1>
+      <Kanban />
+    </div>
+  );
+}
+
+export default App;
